e2f5f6a123ac145f6333583c5b3a7a0eea1118c2
Kevin Lallement <lallement.k@gmail.com>
2024-10-26 22:41:17 +0200

    upd

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -13,6 +13,7 @@ import halloween from 'assets/img/halloween.png';
 import halloweenCard from 'assets/img/halloween-card.png';
 import halloweenBad from 'assets/img/halloween-bad.png';
 import lowLife from 'assets/img/low-life.png';
+import dinovoxLogo from '/dinovox_logo.webp';
 
 export const Quiz = () => {
   const [project, setProject] = useState('1');
@@ -57,45 +58,77 @@ export const Quiz = () => {
           </div>{' '}
           <div className='dinocard'>
             <div className='hcontainer'>
-              <div className='hblock hblock-1'>
-                <div className='devine'>Devine et rugis!</div>
-              </div>
-              <div className='hblock hblock-2'>
-                {' '}
-                <img src={halloween} className='halloween-image' />
-              </div>
-              <div className='hblock hblock-3'>
-                {' '}
-                <form onSubmit={handleSubmit}>
-                  <div>
-                    <label htmlFor='addresses'>Réponse:</label>
-                    <textarea
-                      id=''
-                      placeholder=''
-                      onChange={(e) => {
-                        setAnswer(e.target.value);
-                        setMessage('');
-                      }}
-                      required
-                      style={{
-                        width: '100%',
-                        height: '200px',
-                        padding: '10px'
-                      }}
+              {message != 'Bonne réponse' && (
+                <>
+                  <div className='hblock hblock-1'>
+                    <div className='devine'>Devine et rugis!</div>
+                  </div>
+                  <div className='hblock hblock-2'>
+                    {' '}
+                    <img
+                      src={halloween}
+                      className='halloween-image'
+                      style={{ marginTop: '30px' }}
                     />
                   </div>
-                  <button className='dinoButton '>{'Valider'}</button>
-                </form>
-              </div>
+                  <div className='hblock hblock-2'>
+                    {' '}
+                    <form onSubmit={handleSubmit}>
+                      <div>
+                        <label htmlFor='addresses'>Réponse:</label>
+                        <textarea
+                          id=''
+                          placeholder=''
+                          onChange={(e) => {
+                            setAnswer(e.target.value);
+                            setMessage('');
+                          }}
+                          required
+                          style={{
+                            width: '100%',
+                            height: '200px',
+                            padding: '10px'
+                          }}
+                        />
+                      </div>
+                      <button className='dinoButton '>{'Valider'}</button>
+                    </form>
+                  </div>
+                </>
+              )}
+
               {message === 'Bonne réponse' && (
                 <>
                   <div className='hblock hblock-2'>
-                    <span className='greenGraou'>GRAOU !!! </span>Félicitation,
-                    tu remportes cette magnifique Dinocard collector Graouween !{' '}
+                    <p style={{ fontSize: 'x-large', fontWeight: 'bolder' }}>
+                      Bienvenue à la super fête
+                    </p>
+                    <span className='greenGraou'>GRAOUWEEN</span>
+                    <br />
+                    <p style={{ color: '#6b3f0b' }}>
+                      Félicitations, vous avez trouvé le *Dinopass pour rentrer
+                      dans la grande fête
+                    </p>
                   </div>
-                  <div className='hblock hblock-3'>
+                  <div className='hblock hblock-2'>
                     {' '}
                     <img src={halloweenCard} className='halloween-image' />
+                  </div>
+                  <div className='hblock hblock-1'>
+                    <img
+                      src={dinovoxLogo}
+                      alt='Dinovox Logo'
+                      className='w-64 h-auto'
+                      style={{ margin: 'auto' }}
+                    />
+                  </div>
+                  <div
+                    className='hblock hblock-1'
+                    style={{ fontSize: 'smaller' }}
+                  >
+                    *Le SFT DinoCard collector #11 sera airdrop(distribué
+                    gratuitement) dans votre portefeuille une fois l'évènement
+                    d'Halloween cloturé
                   </div>
                 </>
               )}
