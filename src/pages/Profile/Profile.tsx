aec72ad54e6470cc474133cc687c3f54e146e2d6
Kevin Lallement <lallement.k@gmail.com>
2025-05-09 19:35:07 +0200

    add twitter connect

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { useGetLoginInfo } from 'hooks';
 import { useEffect, useState } from 'react';
 import { message } from 'antd';
+import { is } from '@react-spring/shared';
 
 export type DiscordInfo = {
   discordId: string;
@@ -12,11 +13,16 @@ export type DiscordInfo = {
   globalName: string | null;
 };
 
+export type TwitterInfo = {
+  twitterId: string;
+  username: string;
+  globalName: string | null;
+};
 export type AuthProfile = {
   walletId: number;
   address: string;
-  discordLinked: boolean;
   discord: DiscordInfo | null;
+  twitter: TwitterInfo | null;
 };
 
 export const Profile = () => {
@@ -27,17 +33,12 @@ export const Profile = () => {
   const { tokenLogin } = useGetLoginInfo();
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const discordLinked = params.get('discord') === 'linked';
 
-  useEffect(() => {
-    if (discordLinked) {
-      alert('Votre compte Discord est bien lié !');
-    }
-  }, [discordLinked]);
-
+  const [isDiscordLinked, setIsDiscordLinked] = useState(false);
+  const [isTwitterLinked, setIsTwitterLinked] = useState(false);
   useEffect(() => {
     const fetchAuthProfile = async () => {
-      if (!tokenLogin?.nativeAuthToken) {
+      if (!tokenLogin || (isDiscordLinked && isTwitterLinked)) {
         console.warn('No auth token found');
         return;
       }
@@ -56,6 +57,9 @@ export const Profile = () => {
 
         const data = await res.json();
         setProfile(data);
+        setIsDiscordLinked(data.discord !== null);
+        setIsTwitterLinked(data.twitter !== null);
+
         console.log('Authenticated user profile:', data);
       } catch (err) {
         console.error('Error fetching auth profile:', err);
@@ -64,13 +68,12 @@ export const Profile = () => {
     };
 
     fetchAuthProfile();
-  }, [tokenLogin]);
+  }, [tokenLogin, isDiscordLinked, isTwitterLinked]);
 
   const handleConnectDiscord = async () => {
     if (!tokenLogin) {
       return;
     }
-
     const res = await fetch('https://devnet-api.dinovox.com/auth/discord', {
       method: 'POST',
       headers: {
@@ -84,8 +87,64 @@ export const Profile = () => {
       window.location.href = data.url; // redirection vers Discord OAuth
     }
   };
+  const handleDisconnectDiscord = async () => {
+    if (!tokenLogin) {
+      return;
+    }
 
-  if (loading) {
+    const res = await fetch('https://devnet-api.dinovox.com/auth/discord', {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${tokenLogin.nativeAuthToken}`
+      }
+    });
+
+    const data = await res.json();
+    console.log('Discord unlink response:', data);
+
+    if (data) {
+      setIsDiscordLinked(false);
+    }
+  };
+
+  const handleConnectTwitter = async () => {
+    if (!tokenLogin) {
+      return;
+    }
+    const res = await fetch('https://devnet-api.dinovox.com/auth/twitter', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${tokenLogin.nativeAuthToken}`
+      }
+    });
+
+    const data = await res.json();
+
+    if (data.url) {
+      window.location.href = data.url; // redirection vers Discord OAuth
+    }
+  };
+  const handleDisconnectTwitter = async () => {
+    if (!tokenLogin) {
+      return;
+    }
+
+    const res = await fetch('https://devnet-api.dinovox.com/auth/twitter', {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${tokenLogin.nativeAuthToken}`
+      }
+    });
+
+    const data = await res.json();
+    console.log('Twitter unlink response:', data);
+
+    if (data) {
+      setIsTwitterLinked(false);
+    }
+  };
+
+  if (loading || !profile) {
     return <div>Loading...</div>;
   }
   return (
@@ -93,10 +152,27 @@ export const Profile = () => {
       <PageWrapper>
         <div className='dinocard-wrapper  rounded-xl bg-white flex-col-reverse sm:flex-row items-center h-full w-full'>
           <div className='navigation-cards '>
-            {profile?.discordLinked ? (
-              <>GM {profile.discord?.username}</>
+            {profile?.discord ? (
+              <>
+                GM {profile.discord?.username}
+                <button onClick={handleDisconnectDiscord}>
+                  Disconnect Discord
+                </button>
+              </>
             ) : (
               <button onClick={handleConnectDiscord}>Connect Discord</button>
+            )}
+          </div>{' '}
+          <div className='navigation-cards '>
+            {profile?.twitter ? (
+              <>
+                GM {profile.twitter?.username}
+                <button onClick={handleDisconnectTwitter}>
+                  Disconnect Twitter
+                </button>
+              </>
+            ) : (
+              <button onClick={handleConnectTwitter}>Connect Twitter</button>
             )}
           </div>
         </div>
